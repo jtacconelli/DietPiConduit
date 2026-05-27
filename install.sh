15b09aa6049d5f17b726581abfb66cd746eb6a22
jtacconelli <joe@tacconelli.net>
2026-05-27 11:52:47 -0400

    Update install.sh for Tailscale installation options
    Added shebang and improved installation prompts for Tailscale modes.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+!#/bin/bash
+
 wget -O conduit-installer.deb https://gitlab.com/api/v4/projects/famedly%2Fconduit/jobs/artifacts/master/raw/aarch64-unknown-linux-musl.deb?job=artifacts
 chmod +x conduit-installer.deb
 sudo apt install -y ./conduit-installer.deb
@@ -22,9 +24,7 @@ sudo tailscale up
 
 read -p "Would you like to install this in tailscale private mode? (y/n): " choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-    echo "Installing in Tailscale private mode..."
     tailscale serve --bg --https=443 http://127.0.0.1:6167
 else
-    echo "Installing in public mode..."
     tailscale funnel --bg --https=443 / http://127.0.0.1:6167
 fi
